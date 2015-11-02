c6ee586620d35e590e2d5ce6b06f6369eb350d86
agustin <agustin.herranz@gmail.com>
2015-11-02 18:53:13 +0100

    First revision ' '$ alembic revision -m "Initial revision" '

---
+++ server/alembic/versions/33424076bb8_20151102184915_initial_revision.py
@@ -0,0 +1,24 @@
+"""Initial revision
+
+Revision ID: 33424076bb8
+Revises: 
+Create Date: 2015-11-02 18:49:15.946694
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '33424076bb8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
