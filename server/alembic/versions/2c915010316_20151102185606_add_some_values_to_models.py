7ea5d058dd88440c51945f2f696354547646de23
agustin <agustin.herranz@gmail.com>
2015-11-02 18:57:03 +0100

    Add values to db ' '$ alembic revision -m "Add some values to models" ''

---
+++ server/alembic/versions/2c915010316_20151102185606_add_some_values_to_models.py
@@ -0,0 +1,24 @@
+"""Add some values to models
+
+Revision ID: 2c915010316
+Revises: 33424076bb8
+Create Date: 2015-11-02 18:56:06.370451
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2c915010316'
+down_revision = '33424076bb8'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
